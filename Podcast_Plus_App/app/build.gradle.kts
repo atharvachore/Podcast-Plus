db39bfb2a9b1e4366e148ab113ea4ab650c0304c
Sarcasm1776 <swayam.snkr@gmail.com>
2023-11-09 16:05:45 +0530

    Podcast App
    Basic structure of the app

--- Podcast_Plus_App/app/build.gradle.kts
+++ Podcast_Plus_App/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.playcast"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.playcast"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -59,11 +59,17 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.room:room-common:2.6.0")
+    implementation("androidx.room:room-ktx:2.6.0")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
--- Podcast_Plus_App/app/src/main/java/com/example/playcast/MainActivity.kt
+++ Podcast_Plus_App/app/src/main/java/com/example/playcast/MainActivity.kt
@@ -1,15 +1,42 @@
 package com.example.playcast
 
+import android.content.Context
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.*
 import com.example.playcast.ui.theme.PlaycastTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +49,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    playAudio(this)
                 }
             }
         }
@@ -30,17 +57,64 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PlaycastTheme {
-        Greeting("Android")
+fun playAudio(context: Context) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+            Text(text = "PODCAST",
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                color = Color(0xFF6a3ef9),
+                fontWeight = FontWeight.Bold,
+                fontSize = 36.sp,
+                style = MaterialTheme.typography.headlineLarge,
+                letterSpacing = 0.1.em
+            )
+        }
+        Column( modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())) {
+            Card(
+                //elevation = 12.dp,
+                border = BorderStroke(1.dp, Color.Magenta),
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .height(250.dp)
+            ) {
+                val mp: MediaPlayer = MediaPlayer.create(context, R.raw.audio)
+                Column (
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.img),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(150.dp)
+                            .width(200.dp)
+                    )
+                    Text(
+                        text = "Gaur Gopal Das",
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(start =20.dp, end = 20.dp)
+                    )
+                    Row(){
+                        IconButton(onClick = { mp.start() }, modifier = Modifier.size(35.dp)) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.play),
+                                contentDescription = ""
+                            )
+                        }
+                        IconButton(onClick = { mp.pause() }, modifier = Modifier.size(35.dp)) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.pause),
+                                contentDescription = ""
+                            )
+                        }
+                    }
+                }
+            }
+            //Copy paste previous card for more entries
+        }
     }
 }
